4ceeee5c332dddee1b1be319c640af6fc0ea0f5b
UnknownAD <59281997+UnknownAD@users.noreply.github.com>
2020-09-11 13:57:45 -0400

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,16 @@
+public class s{
+        public static void main(String [] args){
+        System.out.println("HI there");
+        var primal=17;
+        var edit=Integer.toString(primal);
+        System.out.println(edit);
+        method("22");
+        }
+        static void method(String tr){
+        int stdin=Integer.parseInt(tr);
+        int proc=stdin+5;
+        String stdout=Integer.toString(proc);
+        System.out.println(proc);
+}
+}
+
